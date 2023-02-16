279e74db87d3f7b1a0daabde828825c01617ff98
Kevin9921 <55557426+Kevin9921@users.noreply.github.com>
2023-02-16 12:29:50 -0500

    Add files via upload

--- Communication/ClientTest2.py
+++ Communication/ClientTest2.py
@@ -3,6 +3,8 @@ import cv2, imutils, socket
 import numpy as np
 import time
 import base64
+import struct
+from datetime import datetime
 
 BUFF_SIZE = 524288
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
@@ -13,14 +15,51 @@ port = 9999
 message = b'Hope you get this'
 
 client_socket.sendto(message,(host_ip,port))
+previous_timeframe = 0
+new_timeframe = 0
+
+
 
 while True:
-	packet,temp = client_socket.recvfrom(BUFF_SIZE)
+
+	# Recieve packet
+	full_packet,temp = client_socket.recvfrom(BUFF_SIZE)
+
+	#get header
+	udp_header = full_packet[:8]
+
+	#get data
+	packet = full_packet[8:]
+
+	#unpack header
+	udp_header = struct.unpack('d', udp_header)
+
+	#decode video
 	data = base64.b64decode(packet,' /')
 	stringData = np.frombuffer(data, dtype=np.uint8) 
 	#npdata = np.fromstring(data,dtype=np.uint8)
 	frame = cv2.imdecode(stringData,1)
-	cv2.imshow("RECEIVING VIDEO",frame)
+	
+	# Used to get FPS
+	new_timeframe = time.time()
+	fps = 1/(new_timeframe-previous_timeframe)
+	previous_timeframe=new_timeframe
+	fps=int(fps)
+
+	# used to get latency
+	dt = datetime.now()
+	ts = datetime.timestamp(dt)
+
+	# putting text on the window
+	cv2.putText(frame,str(fps),(10,30), cv2.FONT_HERSHEY_SIMPLEX,1,(25,255,0),4)
+	cv2.putText(frame, "S Time: " + str(udp_header[0]) + "s",(10,60), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(25,255,0), 2)
+	cv2.putText(frame, "C Time: " + str(ts) + "s",(10,80) , cv2.FONT_HERSHEY_SIMPLEX, 0.5,(25,255,0), 2)
+	cv2.putText(frame, "Delay: " + str(ts-udp_header[0]) + "s",(10,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(25,255,0), 2)
+
+	# Display video
+	cv2.imshow("CLIENT VIDEO",frame)
+	
+	# Press q to exit the window
 	key = cv2.waitKey(1) & 0xFF
 	if key == ord('q'):
 		client_socket.close()
--- Communication/ServerTest2.py
+++ Communication/ServerTest2.py
@@ -1,8 +1,10 @@
 # This is server code to send video frames over UDP
 import cv2, imutils, socket
-import numpy as np
+
 import time
 import base64
+from datetime import datetime
+import struct
 
 
 BUFF_SIZE = 524288
@@ -17,20 +19,58 @@ server_socket.bind(socket_address)
 print('Listening at:',socket_address)
 
 vid = cv2.VideoCapture(0)
+vid.set(cv2.CAP_PROP_FPS, 20)
+
+previous_timeframe = 0
+new_timeframe = 0
+
+def make_720p():
+    frame.set(3, 1280)
+    frame.set(4, 720)
+
 
 while True:
 	msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
 	print('connected from',client_addr)
 	
+	# open vid
 	while(vid.isOpened()):
 		temp,frame = vid.read()
-		frame = imutils.resize(frame, width = 600)
+		
+		fps2 = int(vid.get(cv2.CAP_PROP_FPS))
+		print("fps:", fps2)
+
+		# resize vid
+		frame = imutils.resize(frame, width = 500)
 		#frame = cv2.resize(frame, (720, 480))
+
+		#downscale quality
 		encoded,buffer = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
+
+		#encode to base64 (bytes)
 		message = base64.b64encode(buffer)
+
+		# get current time in seconds
+		dt = datetime.now()
+		ts = datetime.timestamp(dt)
+
+		# pack time in header
+		udp_header = struct.pack('d', ts)
+
+		# add header and franes and send
+		message = udp_header + message
 		server_socket.sendto(message,client_addr)
-	
-		cv2.imshow('TRANSMITTING VIDEO',frame)
+		
+		# math to get FPS
+		new_timeframe = time.time()
+		fps = 1/(new_timeframe-previous_timeframe)
+		previous_timeframe=new_timeframe
+		fps=int(fps)
+		
+		# put fps on screen
+		cv2.putText(frame,str(fps),(10,30), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,1,(25,255,0),4)
+		
+		cv2.imshow('SERVER VIDEO',frame)
 		key = cv2.waitKey(1) & 0xFF
 		if key == ord('q'):
 			server_socket.close()
