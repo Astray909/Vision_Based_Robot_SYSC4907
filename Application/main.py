6873cd6af4dbacc0eeb5227aefe00264bfa117ba
brnjamino <benransom99@gmail.com>
2023-02-24 11:19:57 -0500

    Manual control should be functional now

--- Application/main.py
+++ Application/main.py
@@ -23,6 +23,7 @@ class Window(QWidget):
          buttonEmergStop = QPushButton()
          buttonEmergStop.setText("STOP")
          buttonEmergStop.setSizePolicy(QSizePolicy.Preferred,QSizePolicy.Expanding)
+         buttonEmergStop.clicked.connect(self.emerg_stop)
          grid.addWidget(buttonEmergStop,5,5,2,2)
 
 
@@ -35,18 +36,19 @@ class Window(QWidget):
 
          buttonForward.setText("Forward")
          buttonForward.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
-         buttonForward.clicked.connect(self.button_was_clicked)
-
-
+         buttonForward.clicked.connect(self.forward_was_clicked)
          grid.addWidget(buttonForward,5,2)
          buttonStop.setText("Stop")
          buttonStop.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+         buttonStop.clicked.connect(self.stop_was_clicked)
          grid.addWidget(buttonStop,6,2)
          buttonTurnLeft.setText("Turn Left")
          buttonTurnLeft.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+         buttonTurnLeft.clicked.connect(self.left_was_clicked)
          grid.addWidget(buttonTurnLeft, 5,1,2,1)
          buttonTurnRight.setText("Turn Right")
          buttonTurnRight.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+         buttonTurnRight.clicked.connect(self.right_was_clicked)
          grid.addWidget(buttonTurnRight, 5,3,2,1)
 
 
@@ -101,10 +103,32 @@ class Window(QWidget):
          error.showMessage(msg)
 
 
-      def button_was_clicked(self):
+      def forward_was_clicked(self):
           keyboard = pynput.keyboard.Controller()
           keyboard.press('w')
-          print('w')
+          print('Forward ')
+      def right_was_clicked(self):
+          keyboard = pynput.keyboard.Controller()
+          keyboard.press('D')
+          print('Right ')
+
+      def left_was_clicked(self):
+         keyboard = pynput.keyboard.Controller()
+         keyboard.press('A')
+         print('Left ')
+
+      def stop_was_clicked(self):
+         keyboard = pynput.keyboard.Controller()
+         keyboard.press('X')
+         print('Stop ')
+
+
+      def emerg_stop(self):
+         keyboard = pynput.keyboard.Controller()
+         keyboard.press('X')
+         print('EMERGENCY STOP')
+
+
 if __name__ == '__main__':
    App = QApplication(sys.argv)
    window = Window()
