248aae70538723d3c9138e519507ee1c4ce27efd
Astray909 <justin9.ij@gmail.com>
2023-03-02 15:44:59 -0500

    added diameter setting to camera tracking

--- object_tracking/object_tracking.py
+++ object_tracking/object_tracking.py
@@ -56,74 +56,74 @@ def tracking():
             tracker = cv2.TrackerCSRT_create()
  
     # Read video
-    # video = cv2.VideoCapture("input.mp4")
-    video = cv2.VideoCapture(0) # for using CAM
- 
+    # cap = cv2.VideoCapture("input.mp4")
+    cap = cv2.VideoCapture(0) # for using CAM
+
     # Exit if video not opened.
-    if not video.isOpened():
+    if not cap.isOpened():
         print("Could not open video")
         sys.exit()
  
     # Read first frame.
-    ok, frame = video.read()
+    ok, frame = cap.read()
     if not ok:
         print ('Cannot read video file')
         sys.exit()
-     
-    # Define an initial bounding box
-    bbox = (287, 23, 86, 320)
 
-    # ROI box background image
-    img = cv2.imread("data/roi_sel/roi_sel_img_0.jpg")
- 
-    # Uncomment the line below to select a different bounding box
-    bbox = cv2.selectROI(img, False)
- 
-    # Initialize tracker with first frame and bounding box
-    ok = tracker.init(frame, bbox)
- 
+    # Create CSRT tracker object
+    tracker = cv2.TrackerCSRT_create()
+
+    # Set initial bounding box coordinates
+    bbox = None
+
     while True:
-        # Read a new frame
-        ok, frame = video.read()
-        if not ok:
+        # Capture frame from webcam
+        ret, frame = cap.read()
+
+        # Check if frame was successfully captured
+        if not ret:
             break
-         
-        # Start timer
-        timer = cv2.getTickCount()
- 
-        # Update tracker
-        ok, bbox = tracker.update(frame)
- 
-        # Calculate Frames per second (FPS)
-        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
- 
-        # Draw bounding box
-        if ok:
-            # Tracking success
-            p1 = (int(bbox[0]), int(bbox[1]))
-            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            print(p1)
-            print(p2)
-            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
-        else :
-            # Tracking failure
-            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
- 
-        # Display tracker type on frame
-        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
-     
-        # Display FPS on frame
-        cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
-        # Display result
-        cv2.imshow("Tracking", frame)
- 
-        # Exit if ESC pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'): # if press q
+
+        # If bounding box is not set, prompt user to select object to track
+        if bbox is None:
+            # ROI box background image
+            img = cv2.imread("data/roi_sel/roi_sel_img_0.jpg")
+        
+            # Uncomment the line below to select a different bounding box
+            bbox = cv2.selectROI(img, False)
+        
+            # Initialize tracker with first frame and bounding box
+            tracker.init(frame, bbox)
+
+        # Update tracker with new frame
+        success, bbox = tracker.update(frame)
+
+        # If tracking was successful, draw bounding box around object
+        if success:
+            # Calculate size of object in camera's field of view based on distance
+            distance = 100  # Example distance in cm from camera
+            object_width = bbox[2]  # Width of object in pixels
+            fov_width = 60  # Example field of view width in degrees
+            pixel_width = frame.shape[1]  # Width of frame in pixels
+            size_in_fov = (object_width / pixel_width) * fov_width
+            actual_size = (size_in_fov / 360) * (2 * distance * 100)  # Multiply by 2 to get diameter of object
+            
+            # Draw bounding box with adjusted size
+            bbox = tuple(map(int, bbox))
+            cv2.rectangle(frame, bbox, (0, 255, 0), 2)
+            cv2.putText(frame, f"Size: {actual_size:.2f} cm", (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        # Display frame with bounding box
+        cv2.imshow("Frame", frame)
+
+        # Exit loop if 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    video.release()
+
+    # Release resources
+    cap.release()
     cv2.destroyAllWindows()
- 
+
 if __name__ == '__main__' :
     save_frame_camera_key(0, 'data/roi_sel', 'roi_sel_img')
     tracking()
-    
