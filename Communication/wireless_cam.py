39df22bf5ed3c4afe306a20b640aa66a68881277
Astray909 <justin9.ij@gmail.com>
2023-03-09 22:40:01 -0500

    updated code

--- Communication/wireless_cam.py
+++ Communication/wireless_cam.py
@@ -21,5 +21,18 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    camera.start_recording('/dev/null', format='h264')
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    camera.resolution = (640, 480)
+    camera.framerate = 24
+    camera.start_preview()
+    app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
+
+    def gen(camera):
+        while True:
+            frame = camera.get_frame()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    @app.route('/video_feed')
+    def video_feed():
+        return Response(gen(camera),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
