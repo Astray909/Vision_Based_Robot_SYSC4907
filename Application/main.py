e0bd060a7e4fe21bc0d2b48506d52b8ccac8d5f1
brnjamino <benransom99@gmail.com>
2023-03-15 12:06:43 -0400

    Mode switching should work(hopefully) still figuring out camera situation

--- Application/main.py
+++ Application/main.py
@@ -6,8 +6,11 @@ from PyQt5.QtMultimedia import *
 from PyQt5.QtMultimediaWidgets import*
 from PyQt5.QtWidgets import *
 import pynput
+from time import sleep
 
 class Window(QWidget):
+      #states: 0=manual, 1=hand gesture, 2=object tracking, 3=temporary diasble
+      states=0
 
       def __init__(self):
          super().__init__()
@@ -63,12 +66,15 @@ class Window(QWidget):
          buttonModeManual.setText("Manual Control")
          buttonModeManual.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
          grid.addWidget(buttonModeManual, 1, 5, 1, 2)
+         buttonModeManual.clicked.connect(self.switch_to_manual)
          buttonModeTracking.setText("Automatic Tracking ")
          buttonModeTracking.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
          grid.addWidget(buttonModeTracking, 2, 5, 1, 2)
+         buttonModeTracking.clicked.connect(self.switch_to_object)
          buttonModeHand.setText("Gesture Control")
          buttonModeHand.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
          grid.addWidget(buttonModeHand, 3, 5, 1, 2)
+         buttonModeHand.clicked.connect(self.switch_to_hand)
 
 
          self.available_cameras =QCameraInfo.availableCameras()
@@ -104,29 +110,98 @@ class Window(QWidget):
 
 
       def forward_was_clicked(self):
-          keyboard = pynput.keyboard.Controller()
-          keyboard.press('w')
-          print('Forward ')
+         if Window.states==0:
+            keyboard = pynput.keyboard.Controller()
+            keyboard.press('w')
+            print('Forward ')
+         elif Window.states==3:
+            print('On timeout')
+         else:
+            print('not in manual mode')
       def right_was_clicked(self):
-          keyboard = pynput.keyboard.Controller()
-          keyboard.press('D')
-          print('Right ')
+         if Window.states == 0:
+             keyboard = pynput.keyboard.Controller()
+             keyboard.press('D')
+             print('Right ')
+         elif Window.states==3:
+            print('On timeout')
+         else:
+            print('not in manual mode')
 
       def left_was_clicked(self):
-         keyboard = pynput.keyboard.Controller()
-         keyboard.press('A')
-         print('Left ')
+         if Window.states == 0:
+            keyboard = pynput.keyboard.Controller()
+            keyboard.press('A')
+            print('Left ')
+         elif Window.states==3:
+            print('On timeout')
+         else:
+            print('not in manual mode')
 
       def stop_was_clicked(self):
-         keyboard = pynput.keyboard.Controller()
-         keyboard.press('X')
-         print('Stop ')
+         if Window.states == 0:
+            keyboard = pynput.keyboard.Controller()
+            keyboard.press('X')
+            print('Stop ')
+         elif Window.states==3:
+            print('On timeout')
+         else:
+            print('not in manual mode')
 
 
       def emerg_stop(self):
+         self.isEnabled(False)
          keyboard = pynput.keyboard.Controller()
          keyboard.press('X')
+         Window.states=3
+         sleep(5)
+         Window.states=0
+         self.isEnabled(True)
          print('EMERGENCY STOP')
+
+
+      def switch_to_manual(self):
+         if Window.states!=3:
+            keyboard = pynput.keyboard.Controller()
+            if Window.states == 1:
+               print('hand to manual')
+            elif Window.states == 2:
+               print('object to manual')
+               keyboard.press('p')
+            Window.states = 0
+            keyboard.press('X')
+         else:
+            print('On timeout')
+
+
+         print('manual')
+      def switch_to_hand(self):
+         if Window.states!=3:
+            keyboard = pynput.keyboard.Controller()
+            if Window.states == 0:
+               print('manual to hand')
+            elif Window.states == 2:
+               print('object to manual')
+               keyboard.press('p')
+            Window.states = 1
+            keyboard.press('X')
+            print('hand')
+         else:
+            print('On timeout')
+
+      def switch_to_object(self):
+         if Window.states!=3:
+            keyboard = pynput.keyboard.Controller()
+            if Window.states == 0:
+             print('manual to object')
+            elif Window.states == 1:
+               print('hand to object')
+               keyboard.press('p')
+            Window.states = 2
+            keyboard.press('X')
+            print('object')
+         else:
+            print('On timeout')
 
 
 if __name__ == '__main__':
